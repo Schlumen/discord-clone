abbff855b4adda7c2108b11260601e309acc2efe
Schlumen <lumen.haendler@web.de>
2025-02-11 16:51:14 +0100

    Invitation

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,54 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface InviteCodePageProps {
+  params: Promise<{ inviteCode: string }>;
+}
+
+const InviteCodePage = async ({ params }: InviteCodePageProps) => {
+  const profile = await currentProfile();
+  if (!profile) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn;
+  }
+
+  const { inviteCode } = await params;
+  if (!inviteCode) return redirect("/");
+
+  const existingServer = await db.server.findFirst({
+    where: {
+      inviteCode,
+      members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (existingServer) return redirect(`/servers/${existingServer.id}`);
+
+  const server = await db.server.update({
+    where: {
+      inviteCode,
+    },
+    data: {
+      members: {
+        create: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+
+  return null;
+};
+
+export default InviteCodePage;
