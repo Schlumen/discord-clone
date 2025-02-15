b7713e2e572881080fff25ee15e6dccbd18831b9
Schlumen <lumen.haendler@web.de>
2025-02-15 12:36:54 +0100

    Get chat messages

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 export const metadata: Metadata = {
   title: "Team Chat App",
@@ -36,7 +37,7 @@ export default function RootLayout({
             storageKey="discord-theme"
           >
             <SocketProvider>
-              {children}
+              <QueryProvider>{children}</QueryProvider>
               <ModalProvider />
             </SocketProvider>
           </ThemeProvider>
