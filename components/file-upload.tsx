b75494304e4714037e3173cfe5239e58c87f6d45
Schlumen <lumen.haendler@web.de>
2025-02-15 11:26:28 +0100

    File upload and emoji

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,9 +1,10 @@
 "use client";
 
-import { X } from "lucide-react";
+import { FileIcon, X } from "lucide-react";
 import Image from "next/image";
 
 import { UploadDropzone } from "@/lib/uploadthing";
+import { useState } from "react";
 
 interface FileUploadProps {
   onChange: (url?: string) => void;
@@ -12,9 +13,9 @@ interface FileUploadProps {
 }
 
 export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
-  const fileType = value?.split(".").pop();
+  const [fileType, setFileType] = useState("");
 
-  if (value && fileType !== "pdf") {
+  if (value && fileType !== "application/pdf") {
     return (
       <div className="relative h-20 w-20 mx-auto">
         <Image
@@ -35,10 +36,34 @@ export const FileUpload = ({ onChange, value, endpoint }: FileUploadProps) => {
     );
   }
 
+  if (value && fileType === "application/pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full shadow-sm absolute -top-2 -right-2"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
   return (
     <UploadDropzone
       endpoint={endpoint}
       onClientUploadComplete={res => {
+        setFileType(res?.[0].type);
         onChange(res?.[0].url);
       }}
       onUploadError={(error: Error) => {
